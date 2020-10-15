adabb123917388366b62917db03fa57380b0b456
soumyadip1995 <30930746+soumyadip1995@users.noreply.github.com>
2020-10-15 21:20:08 +0530

    updated lateral_test.py

--- self driving/controls/control/tests/lateral_test.py
+++ self driving/controls/control/tests/lateral_test.py
@@ -7,8 +7,8 @@ from math import pi
 from lateral_control import calculate_lookahead, calculate_curvature
 from lateral_control import actual_offset
 def main():
-	ego_velocity = 25
-	curr_steering_angle = 194.82
+	ego_velocity = 26
+	curr_steering_angle = 200.82
 	# lookahead_distance = 9.0
 	# lookahead_offset = 1.0
 	# steering_angle_offset = 0.0
